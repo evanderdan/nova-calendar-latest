5da326e334d699b510a2d5b5cbb86618f44b909a
Daniel Newman <daniel.newman@andersen-ev.com>
2026-04-27 12:44:02 +0100

    composer update

--- src/Contracts/CalendarDataProviderInterface.php
+++ src/Contracts/CalendarDataProviderInterface.php
@@ -16,19 +16,24 @@
  
 namespace Wdelfuego\NovaCalendar\Contracts;
 
+use Wdelfuego\NovaCalendar\View\AbstractView as View;
+
 interface CalendarDataProviderInterface
 {
     public function __construct();
+
+    // Will be used for the browser window/tab title
+    public function windowTitle() : string;
     
     // Will be displayed above the calendar
-    public function title() : string;
+    public function titleForView(string $viewSpecifier) : string;
     
     // A 1D array with the names of the seven days of the week, in order of display L -> R
     public function daysOfTheWeek() : array;
     
-    // A multi-dimensional array with all display data for 1 week in the calendar
-    public function calendarWeeks() : array;
-    
     // A multi-dimensional array of event styles, see documentation
     public function eventStyles() : array;
+    
+    // Allows the data provider to customize the data view just before sending it to the front-end
+    public function customizeView(View $view) : View;
 }
