4a318ce5e6e3fc8710fb4884d315c9f36e5883c4
Daniel Newman <daniel.newman@andersen-ev.com>
2026-04-28 15:08:22 +0100

    fix: api and int

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,12 @@
  *     of it in free or paid-for software libraries and packages aimed at developers).
  */
 
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use Wdelfuego\NovaCalendar\Http\Controllers\CalendarController;
+// use Wdelfuego\NovaCalendar\Http\Controllers\CalendarController;
 
-Route::post('/calendar-data', [CalendarController::class, 'getMonthCalendarData']);
+// Route::post('/calendar-data', [CalendarController::class, 'getMonthCalendarData']);
 
 
 Route::get('/', function (NovaRequest $request) {
