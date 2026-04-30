9f3b153f4ab677dafcafdd22470d1e7ed9c3cdfb
Daniel Newman <daniel.newman@andersen-ev.com>
2026-04-30 10:18:03 +0100

    calendar

--- src/Http/Controllers/CalendarController.php
+++ src/Http/Controllers/CalendarController.php
@@ -2,13 +2,13 @@
 
 /*
  * © Copyright 2022 · Willem Vervuurt, Studio Delfuego
- * 
+ *
  * You can modify, use and distribute this package under one of two licenses:
  * 1. GNU AGPLv3
- * 2. A perpetual, non-revocable and 100% free (as in beer) do-what-you-want 
+ * 2. A perpetual, non-revocable and 100% free (as in beer) do-what-you-want
  *    license that allows both non-commercial and commercial use, under conditions.
  *    See LICENSE.md for details.
- * 
+ *
  *    (it boils down to: do what you want as long as you're building and/or
  *     using calendar views, but don't embed this package or a modified version
  *     of it in free or paid-for software libraries and packages aimed at developers).
@@ -16,28 +16,35 @@
 
 namespace Wdelfuego\NovaCalendar\Http\Controllers;
 
-use Illuminate\Foundation\Auth\User;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Carbon;
 use Laravel\Nova\Http\Requests\NovaRequest;
-use Wdelfuego\NovaCalendar\Contracts\CalendarDataProviderInterface;
+use Illuminate\Support\Facades\Log;
 use Wdelfuego\NovaCalendar\DataProvider\MonthCalendar;
+
+use Wdelfuego\NovaCalendar\View\AbstractView as View;
 
 class CalendarController extends BaseController
 {
-    private $request;
-    private $dataProvider;
+    // Must match the hard-coded value in Tool.vue's reload() method
+    const API_PATH_PREFIX = '/nova-vendor/wdelfuego/nova-calendar/';
 
-    public function __construct(NovaRequest $request, CalendarDataProviderInterface $dataProvider)
+    private $dataProviders = [];
+
+    public function __construct()
     {
-        $this->request = $request;
-        /** @var MonthCalendar dataProvider */
-        $this->dataProvider = $dataProvider;
-    }
-
-    /**
-     * Handles the basic presentation of the calendar view
-     */
+        // Load data providers, keyed by uri
+        foreach(config('nova-calendar', []) as $calendarKey => $calendarConfig)
+        {
+            // We are assuming these keys to exist since the Nova Tool
+            // Wdelfuego\NovaCalendar\NovaCalendar does all sorts of checks on initiation
+            // Not sure if that assumption is completely valid but assuming valid config for now
+            $dataProvider = new ($calendarConfig['dataProvider']);
+            $dataProvider->setConfig($calendarConfig);
+            $this->dataProviders[$calendarConfig['uri']] = $dataProvider;
+        }
+    }    
+    
     public function index()
     {
         /** @var User $user */
@@ -47,6 +54,17 @@ class CalendarController extends BaseController
             'pageTitle' => config('nova-calendar.title', 'Nova Calendar'),
             'user' => $user,
         ]);
+    }
+
+
+    protected function getCalendarDataProviderForUri(string $calendarUri)
+    {
+        if(!isset($this->dataProviders[$calendarUri]))
+        {
+            throw new \Exception("Unknown calendar uri: $calendarUri");
+        }
+
+        return $this->dataProviders[$calendarUri];
     }
 
     public function getMonthCalendarData(Request $request)
@@ -79,6 +97,36 @@ class CalendarController extends BaseController
             'weeks' => $this->dataProvider->calendarWeeks(),
             'styles' => array_replace_recursive($this->defaultStyles(), $this->dataProvider->eventStyles()),
         ];
+    }
+
+    public function getCalendarData(NovaRequest $request, string $view = 'month')
+    {
+        $requestUri = substr($request->url(), strlen($request->schemeAndHttpHost()));
+
+        // Get calendar URI from full request URI by ditching the prefix and the last path element (view)
+        $calendarUri = substr($requestUri, strlen(self::API_PATH_PREFIX));
+        $calendarUri = substr($calendarUri, 0, strrpos($calendarUri, '/'));
+
+        $dataProvider = $this->getCalendarDataProviderForUri($calendarUri)->withRequest($request);
+
+        Log::debug('in here - getCalendarData', [$requestUri, $calendarUri, $dataProvider]);
+
+        if($request->query('isInitRequest'))
+        {
+            $dataProvider->setActiveFilterKey($dataProvider->defaultFilterKey());
+        }
+        else
+        {
+            $dataProvider->setActiveFilterKey($request->query('filter'));
+        }
+
+        $view = View::get($view);
+        $view->initFromRequest($request);
+        $view = $dataProvider->customizeView($view);        
+        
+        Log::debug('in here - getCalendarData', [$view]);
+
+        return $view->calendarData($request, $dataProvider);
     }
 
     public function defaultStyles(): array
